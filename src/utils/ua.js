13ff893d9e4042140d6ce2cb18f3ea0c938972fb
Ikarows <1254803307@qq.com>
2022-02-17 10:04:11 +0800

    优化脚手架

--- src/utils/ua.js
+++ src/utils/ua.js
@@ -4,11 +4,10 @@ export default {
     const u = navigator.userAgent
     const isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1 // android终端
     const isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/) // ios终端
-    const isWecath = (u.match(/MicroMessenger/i) === 'micromessenger')
+    const isWecath = (u.match(/MicroMessenger/i) === 'MicroMessenger')
     const isApp = u.indexOf('Html5Plus/') > -1
-
     if (isApp) {
-      return 'wechat'
+      return 'app'
     } else if (isWecath) {
       return 'wechat'
     } else if (isAndroid) {
